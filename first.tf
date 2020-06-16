c4292c72c8f3623f3b202f0ad9c98c8342ad1266
khan <khansoumyadip@gmail.com>
2020-06-16 11:15:47 +0000

    first version

---
+++ first.tf
@@ -0,0 +1,1 @@
+provider "azurerm" {  features {}}resource "azurerm_resource_group" "demo1" {  name     = "testrg1trainer"  location = "eastus"   tags = {      env = "testrg1trainer-demo"  } }resource "azurerm_resource_group" "demo2" {  name     = "testrg2trainer"  location = "southindia"}
