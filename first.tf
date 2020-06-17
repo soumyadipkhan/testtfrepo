2369ff23b56478bbcf2bf97ec7030625f106e462
soumyadipkhan <khansoumyadip@gmail.com>
2020-06-17 23:52:23 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,1 +1,23 @@
-provider "azurerm" {  features {}}resource "azurerm_resource_group" "demo1" {  name     = "testrg1trainer"  location = "eastus"   tags = {      env = "testrg1trainer-demo"  } }resource "azurerm_resource_group" "demo2" {  name     = "testrg2trainer"  location = "southindia"}
+provider "azurerm" {
+  subscription_id = "6c4c8aa9-7619-49c3-90e5-5ed6379206ce"
+  client_id       = "4fa51088-87b1-46a8-aedc-df60fb9c3a27"
+  client_secret   = "ctmgNp_SKCVEUVVKFs.tS~SET~_qN0bW1w"
+  tenant_id       = "662fe93f-685e-4e6a-a8d2-3604267f226f"
+
+
+  features {}
+}
+
+resource "azurerm_resource_group" "demo1" {
+  name     = "testrg1trainer3"
+  location = "eastus"
+  tags = {
+      env = "resource-group-demo"
+  }
+
+}
+
+resource "azurerm_resource_group" "demo2" {
+ name     = "testrg2trainer"
+  location = "southindia"
+}
